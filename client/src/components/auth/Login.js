fda801aa724aaf4a593a9cc07f7773f04403ad7e
AlexA3 <alexpop967@gmail.com>
2021-09-11 00:26:49 +0300

    Added register/login UI

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [loginState, setLoginState] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onChange = (e) => {
+    console.log(e);
+    setLoginState((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const user = {
+      emal: loginState.email,
+      password: loginState.password,
+    };
+
+    console.log(user);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <section className="container">
+      <div className="alert alert-danger">Invalid Credentials</div>
+
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign into your account
+      </p>
+      <form onSubmit={onSubmit} className="form">
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            value={loginState.email}
+            onChange={onChange}
+            id="email"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            minlength="6"
+            value={loginState.password}
+            onChange={onChange}
+            id="password"
+          />
+        </div>
+        <input type="submit" value="Login" className="btn btn-primary" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <a href="register.html">Sign Up</a>
+      </p>
+    </section>
   );
 };
 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,126 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+  const [registerState, setRegisterState] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    errors: {},
+  });
+
+  const onChange = (e) => {
+    setRegisterState((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+
+    Object.keys(registerState.errors).forEach(
+      () => (registerState.errors[e.target.id] = undefined)
+    );
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const newUser = {
+      name: registerState.name,
+      email: registerState.email,
+      password: registerState.password,
+      confirmPassword: registerState.confirmPassword,
+    };
+
+    await axios
+      .post("api/users/register", newUser)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        setRegisterState({ ...registerState, errors: err.response.data });
+      });
+  };
+
   return (
-    <div>
-      <h1>React component</h1>
-    </div>
+    <section className="container">
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Create Your Account
+      </p>
+      <form noValidate onSubmit={onSubmit} className="form">
+        <input
+          id="name"
+          type="text"
+          placeholder="Name"
+          value={registerState.name}
+          onChange={onChange}
+          className={`form-control form-control-lg ${
+            registerState.errors.name ? "is-invalid" : ""
+          }`}
+        />
+        {registerState.errors.name && (
+          <div className="invalid-feedback">{registerState.errors.name}</div>
+        )}
+        <div className="form-group">
+          <input
+            id="email"
+            type="email"
+            placeholder="Email Address"
+            value={registerState.email}
+            onChange={onChange}
+            className={`form-control form-control-lg ${
+              registerState.errors.email ? "is-invalid" : ""
+            }`}
+          />
+          {registerState.errors.email && (
+            <div className="invalid-feedback">{registerState.errors.email}</div>
+          )}
+          <small className="form-text">
+            This site uses Gravatar, so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            id="password"
+            type="password"
+            placeholder="Password"
+            value={registerState.password}
+            onChange={onChange}
+            className={`form-control form-control-lg ${
+              registerState.errors.password ? "is-invalid" : "test"
+            }`}
+          />
+          {registerState.errors.password && (
+            <div className="invalid-feedback">
+              {registerState.errors.password}
+            </div>
+          )}
+        </div>
+        <div className="form-group">
+          <input
+            id="confirmPassword"
+            type="password"
+            placeholder="Confirm Password"
+            value={registerState.confirmPassword}
+            onChange={onChange}
+            className={`form-control form-control-lg ${
+              registerState.errors.confirmPassword ? "is-invalid" : ""
+            }`}
+          />
+          {registerState.errors.confirmPassword && (
+            <div className="invalid-feedback">
+              {registerState.errors.confirmPassword}
+            </div>
+          )}
+        </div>
+        <input type="submit" value="Register" className="btn btn-primary" />
+      </form>
+      <p className="my-1">
+        Already have an account? <Link to="/login">Sign In</Link>
+      </p>
+    </section>
   );
 };
 
